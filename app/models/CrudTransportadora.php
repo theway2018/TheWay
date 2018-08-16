1bccf1dc2190335076d9bd2e3673369e7aeef09c
The way <42278684+theway2018@users.noreply.github.com>
2018-08-15 22:19:56 -0300

    Add files via upload

--- app/models/CrudTransportadora.php
+++ app/models/CrudTransportadora.php
@@ -5,18 +5,16 @@
  * Date: 13/08/2018
  * Time: 13:45
  */
-<?php
-
  require_once "conexao.php";
 
- require_once "caminhoneiro.php";
+ require_once "Transportadora.php";
 
 //DAQUI PARA BAIXO, POR ENQUANTO, ESTÁ COPIADO DO CrudCaminhoneiro
 
 class Crudusuario{
 
     private $conexao;
-    public  $caminhoneiro;
+    public  $Transportadora;
 
     //CONEXÃO COM O BANCO
     public function __construct(){
@@ -24,10 +22,10 @@ class Crudusuario{
     }
 
     //Cadastra o usuario
-    public function salvar(usuario $caminhoneiro){
+    public function salvar(usuario $Transportadora){
 
-        $sql = "INSERT INTO caminhoneiro (nome, email, telefone, senha, rg, cpf, num_antt, num_cnh, categoria_cnh, cod_usuario) 
-                VALUES ({$caminhoneiro->nome}, {$caminhoneiro->sobrenome}, {$caminhoneiro->email}, {$caminhoneiro->telefone}, {$caminhoneiro->senha}, {$caminhoneiro->rg}, {$caminhoneiro->cpf}, {$caminhoneiro->num_antt}, {$caminhoneiro->num_cnh}, {$caminhoneiro->categoria_cnh})";
+        $sql = "INSERT INTO transportadora (nome, email, telefone, senha, razao_social, cnpj); 
+                VALUES ({$Transportadora->nome}, {$Transportadora->email}, {$Transportadora->telefone}, {$Transportadora->senha}, {$Transportadora->razao_social}, {$Transportadora->cnpj}";
 
         $this->conexao->exec($sql);
     }
@@ -36,38 +34,38 @@ class Crudusuario{
     //Busca
     public function getusuario(int $cod_usuario)
     {
-        $consultausuarios->conexao->query("SELECT * FROM caminhoneiro WHERE cod_usuario = $cod_usuario");
+        $consultausuarios->conexao->query("SELECT * FROM transportadora WHERE cod_usuario = $cod_usuario");
         $usuario = $consulta->fetch(PDO::FETCH_ASSOC); //SEMELHANTES JSON ENCODE E DECODE
 
-        return new usuario($usuario['nome'], $usuario['email'], $usuario['telefone'], $usuario['senha'], $usuario['rg'], $usuario['cpf'], $usuario['num_cnh']);
+        return new usuario($usuario['nome'], $usuario['email'], $usuario['telefone'], $usuario['senha'], $usuario['razao_social'], $usuario['cnpj'];
     }
 
     public function getusuarios(){
-        $consulta = $this->conexao->query("SELECT * FROM caminhoneiro");
-        $arrayusuarios = $consulta->fetchAll($this->caminhoneiroTCH_ASSOC);
+        $consulta = $this->conexao->query("SELECT * FROM transportadora");
+        $arrayusuarios = $consulta->fetchAll($this->transportadoraTCH_ASSOC);
 
         //Fabrica de caminhoneiro
         $listausuario = [];
         foreach ($arrayusuarios as $usuarusuario  $listausuario[] = new usuario($usuarios['nome'], $usuarios['sobrenome'], $usuarios['email'],$usuarios['quantidade_telefone'], $usuarios['cod_usuario']){
         }
-        return $this->caminhoneiro;
+        return $this->transportadora;
     }
 
     //Exclui o usuario
     public function excluirusuario(int $x)
     {
-        $this->conexao->exec("DELETE from caminhoneiro where cod_usuario = $x"usuario}
+        $this->conexao->exec("DELETE from transportadora where cod_usuario = $x"usuario}
 
     //Edita as informações do usuario
-    public function editar($id, $nome, $email, $sobrenome, $telefone)
+    public function editar($id, $nome, $email, $telefone)
     {
-        $this->conexao->exec("UPDATE caminhoneiro SET nome = $nome, email = $email, sobrenome =usuarionome, telefone = $telefone WHERE usuario.cod_usuario = $id; ");
+        $this->conexao->exec("UPDATE transportadora SET nome = $nome, email = $email, sobrenome =usuarionome, telefone = $telefone WHERE usuario.cod_usuario = $id; ");
     }
 
     public function login($usuario, $senha){
         $consultausuarios->conexao->query("SELECT * FROM usuario WHERE cod_usuario = $cod_usuario");
         $usuario = $consulta->fetch(PDO::FETCH_ASSOC); //SEMELHANTES JSON ENCODE E DECODE
-        return new usuario($usuario['nome'], $usuario['sobrenome'], $usuario['email'], $usuario['cod_usuario']);
+        return new usuario($usuario['nome'], $usuario['email'], $usuario['cod_usuario']);
 
     }
 
