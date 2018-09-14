a841edb11f7ebfd1ccacd221835257c5df045bf5
The way <42278684+theway2018@users.noreply.github.com>
2018-09-14 16:08:42 -0300

    Add files via upload

--- app/models/CrudCaminhoneiro.php
+++ app/models/CrudCaminhoneiro.php
@@ -16,17 +16,19 @@ class Crudcaminhoneiro{
 
     //Cadastra o usuario caminhoneiro
     public function salvar (caminhoneiro $caminhoneiro){
-        var_dump($caminhoneiro);
-        $sql = "INSERT INTO caminhoneiro (nome, email, telefone, senha, rg, cpf, num_antt, num_cnh, categoria_cnh) 
+        /*var_dump($caminhoneiro);*/
+        $sql = "INSERT INTO caminhoneiro (nome, email, telefone, senha, rg, cpf, num_antt, num_cnh, categoria_cnh)
                 VALUES ('$caminhoneiro->nome', '$caminhoneiro->email', '$caminhoneiro->telefone', '$caminhoneiro->senha', '$caminhoneiro->rg', '$caminhoneiro->cpf', '$caminhoneiro->num_antt', '$caminhoneiro->num_cnh', '$caminhoneiro->categoria_cnh')";
+        /*$sql = 'INSERT INTO caminhoneiro (nome, email, telefone, senha, rg, cpf, num_antt, num_cnh, categoria_cnh) VALUES ('$caminhoneiro->nome', '$caminhoneiro->email', '$caminhoneiro->telefone', '$caminhoneiro->senha', '$caminhoneiro->rg', '$caminhoneiro->cpf', '$caminhoneiro->num_antt', '$caminhoneiro->num_cnh', '$caminhoneiro->categoria_cnh')';*/
 
         $this->conexao->exec($sql);
     }
 
     //Busca usuario caminhoneiro
-    public function getusuario (int $cod_usuario){
+    public function getusuario (int $cod_caminhoneiro){
 
-        $consultausuario->conexao->query("SELECT * FROM caminhoneiro WHERE cod_usuario = $cod_usuario");
+        // Substitui o $consultausuarios por $consulta
+        $consulta->conexao->query("SELECT * FROM caminhoneiro WHERE cod_caminhoneiro= $cod_caminhoneiro");
         $usuario = $consulta->fetch(PDO::FETCH_ASSOC); //SEMELHANTES JSON ENCODE E DECODE
 
         return new usuario($usuario['nome'], $usuario['email'], $usuario['telefone'], $usuario['senha'], $usuario['rg'], $usuario['cpf'], $usuario['num_cnh']);
@@ -52,19 +54,20 @@ class Crudcaminhoneiro{
     //Exclui o usuario
     public function excluirusuario ($x){
 
-        $this->conexao->exec("DELETE from caminhoneiro where cod_usuario = $x");
+        $this->conexao->exec("DELETE from caminhoneiro where cod_caminhoneiro = $x");
 }
 
     //Edita as informações do usuario
     public function editar ($nome, $email, $telefone, $senha, $rg, $cpf, $num_cnh){
 
-        $this->conexao->exec("UPDATE caminhoneiro SET nome = $nome, email = $email, telefone = $telefone, senha = $senha, rg = $rg, cpf = $cpf, num_cnh = $num_cnh WHERE caminhoneiro.cod_usuario = $id; ");
+        $this->conexao->exec("UPDATE caminhoneiro SET nome = $nome, email = $email, telefone = $telefone, senha = $senha, rg = $rg, cpf = $cpf, num_cnh = $num_cnh WHERE caminhoneiro.cod_caminhoneiro = $id; ");
     }
 
     //login
-    public function login ($usuario, $senha, $cod_usuario){
+    public function login ($usuario, $senha, $cod_caminheiro){
 
-        $consultausuarios->conexao->query("SELECT * FROM caminhoneiro WHERE cod_usuario = $cod_usuario");
+        // Substitui o $consultausuarios por $consulta
+        $consulta->conexao->query("SELECT * FROM caminhoneiro WHERE cod_caminhoneiro = $cod_caminheiro");
         $usuario = $consulta->fetch(PDO::FETCH_ASSOC); //SEMELHANTES JSON ENCODE E DECODE
 
         return new usuario($usuario['nome'], $usuario['senha'], $usuario['telefone'], $usuario['senha'], $usuario['rg'], $usuario['cpf'], $usuario['num_cnh']);
